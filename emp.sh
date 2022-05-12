a0786cc66c9042b40768d58d2beaf59e084947ea
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-05-12 23:06:27 +0530

    modified:2

--- emp.sh
+++ emp.sh
@@ -13,10 +13,19 @@ fi
 echo "---------------------------------"
 
 #uc_1
-
+echo "Employee present full day ";
 EMP_WAGE_PER_HOUR=20;
 WORKING_HOUR=8;
 dailywage=$((EMP_WAGE_PER_HOUR * WORKING_HOUR));
 echo "Employee dailywage is :$dailywage";
 echo "-----------------------------------------------"
+
+#UC_2
+echo "Employee present part time";
+
+EMP_WAGE_PER_HOUR=20;
+PART_TIME_WORKING_HOUR=4;
+dailywage=$((EMP_WAGE_PER_HOUR * PART_TIME_WORKING_HOUR));
+echo "Employee dailywage is :$dailywage";
+echo "------------------------------------------------------------"
 
