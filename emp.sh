1b59b8c8b70150d8840fce8ddf6b6706d88c032d
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-05-13 07:52:52 +0530

    modified-4

--- emp.sh
+++ emp.sh
@@ -54,3 +54,13 @@ else
         dailywage=$((EMP_WAGE_PER_HOUR * PART_TIME_WORKING_HOUR));
 echo "Employee dailywage is :$dailywage";
 fi
+echo "----------------------------------------------------------------";
+
+EMP_WAGE_PER_HOUR=20;
+WORKING_HOUR=8;
+EMP_WORKING_DAYS=20;
+PART_TIME_WORKING_HOUR=4;
+mothlywage=$(( (EMP_WAGE_PER_HOUR * WORKING_HOUR )* EMP_WORKING_DAYS));
+echo "Employeee full time monthly wage:$monthlywage";
+mothlywage=$((EMP_WAGE_PER_HOUR * PART_TIME_WORKING_HOUR * EMP_WORKING_DAYS));
+echo "Employee part time monthly wage :$monthlywage";
